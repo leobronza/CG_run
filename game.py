af337e170f58b1e6f5054869df8373f249e3cd3d
Vinicius Moro Sanches <suiciniv.ms@hotmail.com>
2017-01-23 20:47:45 -0200

    arrumando turn

--- game.py
+++ game.py
@@ -58,31 +58,32 @@ def Player():
 # def PlayerKeepMoving(player):
 
 
-def MovGrounds(groundCurrentPlus):
-    if (grounds[groundCurrentPlus].dir == 0):
-        for ground in grounds:
-            for i in range(0, len(ground.listZ)):
-                listz = list(ground.listZ)
-                listz[i] = listz[i] + 0.1
-                ground.listZ = listz
-    elif (grounds[groundCurrentPlus].dir == 1):
-        for ground in grounds:
-            for i in range(0, len(ground.listX)):
-                listx = list(ground.listX)
-                listx[i] = listx[i] - 0.1
-                ground.listX = listx
-    elif (grounds[groundCurrentPlus].dir == 2):
-        for ground in grounds:
-            for i in range(0, len(ground.listZ)):
-                listz = list(ground.listZ)
-                listz[i] = listz[i] - 0.1
-                ground.listZ = listz
-    elif (grounds[groundCurrentPlus].dir == 3):
-        for ground in grounds:
-            for i in range(0, len(ground.listX)):
-                listx = list(ground.listX)
-                listx[i] = listx[i] + 0.1
-                ground.listX = listx
+def MovGrounds(groundCurrentPlus, dead):
+    if not dead:
+        if (grounds[groundCurrentPlus].dir == 0):
+            for ground in grounds:
+                for i in range(0, len(ground.listZ)):
+                    listz = list(ground.listZ)
+                    listz[i] = listz[i] + 0.1
+                    ground.listZ = listz
+        elif (grounds[groundCurrentPlus].dir == 1):
+            for ground in grounds:
+                for i in range(0, len(ground.listX)):
+                    listx = list(ground.listX)
+                    listx[i] = listx[i] - 0.1
+                    ground.listX = listx
+        elif (grounds[groundCurrentPlus].dir == 2):
+            for ground in grounds:
+                for i in range(0, len(ground.listZ)):
+                    listz = list(ground.listZ)
+                    listz[i] = listz[i] - 0.1
+                    ground.listZ = listz
+        elif (grounds[groundCurrentPlus].dir == 3):
+            for ground in grounds:
+                for i in range(0, len(ground.listX)):
+                    listx = list(ground.listX)
+                    listx[i] = listx[i] + 0.1
+                    ground.listX = listx
 
 # Printa os Grounds
 def Grounds():
@@ -97,12 +98,13 @@ def Grounds():
 # Funcao Principal
 def Game():
     glTranslatef(0, -0.5, -5)
-    glRotatef(12, 1, 0, 0)
+    glRotatef(20, 1, 0, 0)
     contaux = 0
     groundCurrent = 1
     margem = 0
     wait4right = False
     wait4left = False
+    dead = False
 
     while True:
         if groundCurrent == 17:
@@ -123,24 +125,24 @@ def Game():
                 quit()
             # Vira camera ao pressinar LEFT
             elif event.type == KEYUP and event.key == K_LEFT:
-                if grounds[groundCurrent].dir != grounds[groundCurrentPlusPlus].dir:
-                    wait4left = True
                 if CanTurn(0, groundCurrent, groundCurrentPlus) == True:
-                    glRotatef(-90, 0, 1, 0)
-                    grounds[groundCurrent].dir = grounds[groundCurrentPlus].dir
-                    contaux = 1.5
+                    wait4left = True
+                else:
+                    dead = True
+
             # Vira camera ao pressinar RIGHT
             elif event.type == KEYUP and event.key == K_RIGHT:
                 if CanTurn(1, groundCurrent, groundCurrentPlus ) == True:
                     wait4right = True
-                    # if margem > 1.5:
-                    #     pass
+                else:
+                    dead = True
+
 
 
 
         if(grounds[groundCurrent].dir == grounds[groundCurrentPlus].dir):
             # Atualizacao posicao dos Grounds
-            MovGrounds(groundCurrentPlus)
+            MovGrounds(groundCurrentPlus, dead)
 
             # Atualiza qual eh o bloco atual
             if contaux >= 3:
@@ -164,9 +166,17 @@ def Game():
                 grounds[groundCurrent].dir = grounds[groundCurrentPlus].dir
                 contaux = 1.5
                 wait4right = False
+                margem = 0
+
+            if wait4left and margem >= 1.5:
+                glRotatef(-90, 0, 1, 0)
+                grounds[groundCurrent].dir = grounds[groundCurrentPlus].dir
+                contaux = 1.5
+                wait4left = False
+                margem = 0
 
             if margem < 1.5:
-                MovGrounds(groundCurrent)
+                MovGrounds(groundCurrent, dead)
                 margem = margem + 0.1
                 # if wait4right:
 
